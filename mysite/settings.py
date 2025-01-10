8e6610b4da94df3d956d37f88936c9d56ead1420
Alan <alanbignon2@gmail.com>
2025-01-10 17:46:35 +0100

    ulr

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = '/home/ubuntu/portfolio/static'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'home/static/home'),
 )
