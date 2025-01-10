eba28b7bef31863ccab13f817c4b8f98f20354c2
Alan <alanbignon2@gmail.com>
2025-01-10 18:41:59 +0100

    debug + hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.75.22.217', 'alanbignon.com']
 
 
 # Application definition
