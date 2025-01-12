de29e889342474ff5ea53a7ada60d6480df9bc2e
Alan <alanbignon2@gmail.com>
2025-01-12 13:29:51 +0100

    fix favicon

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls")),
-    path("favicon.ico", RedirectView.as_view(url='/static/images/favicon.ico'))
+    path("", include("home.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
