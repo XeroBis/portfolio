3a21925862ff23de59402f5291ebba886899aa4a
Alan <alanbignon2@gmail.com>
2025-01-12 13:24:36 +0100

    ajout favicon

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls"))
+    path("", include("home.urls")),
+    path("favicon.ico", RedirectView.as_view(url='/static/images/favicon.ico'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
