3dbee4d95ab43eaece91154ea8d97f9d910c6437
Alan <alanbignon2@gmail.com>
2025-01-16 13:33:22 +0100

    debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ast.literal_eval(os.getenv("ALLOWED_HOSTS"))
 
