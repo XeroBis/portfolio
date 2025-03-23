2be0b7768f3db14f33da525915c4564289b68c3e
Alan <alanbignon2@gmail.com>
2025-03-23 22:31:53 +0100

    ajout bouton de download, upload et reset des données

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("export_data/", views.download_data_json, name="export_data"),
+    path("import_data/", views.import_data_json, name="import_data"),
+    path("reset_data/", views.reset_data, name="reset_data"),
     path("", views.home, name="home"),
 ]
