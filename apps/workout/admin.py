706514dbe61854cf6b13e081d7451df078efbc51
Alan <alanbignon2@gmail.com>
2025-12-01 23:11:57 +0100

    add one exerice to admin

--- apps/workout/admin.py
+++ apps/workout/admin.py
@@ -5,6 +5,7 @@ from .models import (
     Equipment,
     Exercice,
     MuscleGroup,
+    OneExercice,
     StrengthSeriesLog,
     TypeWorkout,
     Workout,
@@ -68,13 +69,20 @@ class CardioSeriesLogInline(admin.TabularInline):
     ordering = ["exercise__name", "series_number"]
 
 
+class OneExerciceInline(admin.TabularInline):
+    model = OneExercice
+    extra = 1
+    fields = ["position", "name"]
+    ordering = ["position"]
+
+
 @admin.register(Workout)
 class WorkoutAdmin(admin.ModelAdmin):
     list_display = ["date", "type_workout", "duration", "get_exercise_count"]
     search_fields = ["date", "type_workout__name_workout"]
     list_filter = ["date", "type_workout", "duration"]
     fieldsets = ((None, {"fields": ("date", "type_workout", "duration")}),)
-    inlines = [StrengthSeriesLogInline, CardioSeriesLogInline]
+    inlines = [OneExerciceInline, StrengthSeriesLogInline, CardioSeriesLogInline]
 
     @admin.display(description="Exercises")
     def get_exercise_count(self, obj):
